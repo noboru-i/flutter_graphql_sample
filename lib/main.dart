4b6b61ce45bb28fcdc752c8ec172de54e36b1942
noboru-i <ishikura.noboru@gmail.com>
2020-08-08 15:46:59 +0900

    works query and mutate

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:artemis/artemis.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttergraphqlsample/generated/graphql_api.graphql.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
 void main() {
   runApp(MyApp());
@@ -28,12 +31,39 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<List$Query$Post> posts = [];
 
-  void _incrementCounter() {
+  @override
+  void initState() {
+    super.initState();
+
+    _init();
+  }
+
+  Future<void> _init() async {
+    final client = GraphQLApiClient();
+
+    final query = ListQuery();
+    final result = await client.query(query);
+    final data = query.parse(result.data);
     setState(() {
-      _counter++;
+      posts = data.posts;
     });
+  }
+
+  Future<void> _upvote(int postId) async {
+    final client = GraphQLApiClient();
+
+    final query = UpvotePostMutation(
+      variables: UpvotePostArguments(
+        postId: postId,
+      ),
+    );
+    final result = await client.mutate(query);
+    final data = query.parse(result.data);
+    print('data = ${data.upvotePost.toJson()}');
+
+    _init();
   }
 
   @override
@@ -42,25 +72,66 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+      body: ListView.builder(
+        itemCount: posts.length,
+        itemBuilder: (context, index) => _buildListItem(index),
       ),
     );
+  }
+
+  Widget _buildListItem(int index) {
+    final item = posts[index];
+    return ListTile(
+      title: Text(item.title),
+      trailing: Text('vote: ${item.votes}'),
+      onTap: () {
+        _upvote(item.id);
+      },
+    );
+  }
+}
+
+// ref: https://aakira.app/blog/2020/06/flutter-graphql/
+class GraphQLApiClient {
+  GraphQLApiClient()
+      : graphQLClient = GraphQLClient(
+          cache: InMemoryCache(),
+          link: HttpLink(
+            uri: 'http://10.0.2.2:3000/graphql',
+          ),
+        );
+
+  final GraphQLClient graphQLClient;
+
+  Future<QueryResult> query(GraphQLQuery query) async {
+    final QueryResult result = await graphQLClient.query(QueryOptions(
+      documentNode: query.document,
+      variables: query.variables?.toJson(),
+    ));
+
+    if (!result.hasException) {
+      print(result.exception);
+      for (final GraphQLError error in result.exception.graphqlErrors) {
+        print(error.message);
+      }
+    }
+
+    return result;
+  }
+
+  Future<QueryResult> mutate(GraphQLQuery query) async {
+    final QueryResult result = await graphQLClient.mutate(MutationOptions(
+      documentNode: query.document,
+      variables: query.variables?.toJson(),
+    ));
+
+    if (!result.hasException) {
+      print(result.exception);
+      for (final GraphQLError error in result.exception.graphqlErrors) {
+        print(error.message);
+      }
+    }
+
+    return result;
   }
 }
